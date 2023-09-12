3c64744b0b1ce652c2c0193e96af0982234718f1
sandeeepmangukiya <s.mangukiya@web.de>
2023-09-12 14:31:00 +0200

    modified package and Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
     return (
