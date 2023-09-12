ad9c3416070187f99de9645784cb25304b568e2e
sandeeepmangukiya <s.mangukiya@web.de>
2023-09-12 08:44:14 +0200

    modified package and Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
     return (
         <>
             <footer className="bg-dark text-white text-center py-4">
-                <p>Sandeep Mangukiya | Parampara Fab</p>
+                <section>
+                    <ul>
+                        <li>
+                            <NavLink to={{pathname:"https://www.facebook.com"}} target="_blank">
+                                <FontAwesomeIcon icon={faFacebook} size="2x" />
+                            </NavLink>
+                        </li>
+                    </ul>
+                </section>
+                <section>
+                    <p>Sandeep Mangukiya | Parampara Fab</p>
+                </section>
             </footer>
         </>
     );
